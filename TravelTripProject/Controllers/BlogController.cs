afe5d0d1a0a2ac1eebef45387b6e28cb0f8745ad
Arda <ardabuyuk2738@gmail.com>
2026-03-03 20:02:17 +0300

    Blogs has added.

---
+++ TravelTripProject/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelTripProject.Models.Siniflar;
+
+namespace TravelTripProject.Controllers;
+
+public class BlogController : Controller
+{
+    Context c = new Context();
+    public IActionResult Index()
+    {
+        var blogs = c.Blogs.ToList();
+        return View(blogs);
+    }
+    public IActionResult BlogDetay(int id)
+    {
+        var blogBul = c.Blogs.Where(x => x.ID == id).ToList();
+        return View(blogBul);
+    }
+}
