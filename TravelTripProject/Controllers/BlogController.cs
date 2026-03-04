fa761d0ba7038469d16d828d50d688aa529a46e4
Arda Büyük <ardabuyuk2738@gmail.com>
2026-03-04 11:29:26 +0300

    20th video completed.

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -6,14 +6,22 @@ namespace TravelTripProject.Controllers;
 public class BlogController : Controller
 {
     Context c = new Context();
+    BlogYorum by = new BlogYorum();
     public IActionResult Index()
     {
-        var blogs = c.Blogs.ToList();
-        return View(blogs);
+        //var blogs = c.Blogs.ToList();
+        by.Deger1 = c.Blogs.ToList();
+        by.Deger3 = c.Blogs.Take(3).ToList();
+        return View(by);
     }
+    
     public IActionResult BlogDetay(int id)
     {
-        var blogBul = c.Blogs.Where(x => x.ID == id).ToList();
-        return View(blogBul);
+
+        //var blogBul = c.Blogs.Where(x => x.ID == id).ToList();
+        by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+        by.Deger2 = c.Yorumlars.Where(x => x.BlogID == id).ToList();
+
+        return View(by);
     }
 }
