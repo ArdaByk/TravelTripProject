eecb4ea6d634216855f0adce49bb005e8367c73a
Arda <ardabuyuk2738@gmail.com>
2026-03-17 20:59:28 +0300

    26th video completed.

--- TravelTripProject/Controllers/DefaultController.cs
+++ TravelTripProject/Controllers/DefaultController.cs
@@ -1,17 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TravelTripProject.Models.Siniflar;
 
-namespace TravelTripProject.Controllers
+namespace TravelTripProject.Controllers;
+
+public class DefaultController : Controller
 {
-    public class DefaultController : Controller
+    Context c = new Context();
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        var degerler = c.Blogs.ToList();
+        return View(degerler);
+    }
 
-        public ActionResult About()
-        {
-            return View();
-        }
+    public ActionResult About()
+    {
+        return View();
+    }
+
+    public PartialViewResult Partial1()
+    {
+        var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+        return PartialView(degerler);
+    }
+
+    public PartialViewResult Partial2()
+    {
+        var degerler = c.Blogs.Where(x => x.ID == 1).ToList();
+        return PartialView(degerler);
+    }
+
+    public PartialViewResult Partial3()
+    {
+        var deger = c.Blogs.ToList();
+
+        return PartialView(deger);
     }
 }
