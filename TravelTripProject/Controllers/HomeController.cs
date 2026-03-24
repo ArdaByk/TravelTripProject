193587d10fd8dc743806e21cb0b9023fe9b396ae
Arda Büyük <ardabuyuk2738@gmail.com>
2026-03-24 18:01:06 +0300

    Bugs fixed.

--- TravelTripProject/Controllers/HomeController.cs
+++ TravelTripProject/Controllers/HomeController.cs
@@ -1,14 +1,17 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TravelTripProject.Models;
+using TravelTripProject.Models.Siniflar;
 
 namespace TravelTripProject.Controllers
 {
     public class HomeController : Controller
     {
+        Context c = new Context();
         public IActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
         }
 
         public IActionResult Privacy()
@@ -20,6 +23,24 @@ namespace TravelTripProject.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial2()
+        {
+            var degerler = c.Blogs.Where(x => x.ID == 1).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.ToList();
+
+            return PartialView(deger);
         }
     }
 }
